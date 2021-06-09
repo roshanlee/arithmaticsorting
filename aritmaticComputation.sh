6f1a66b0de2439b96cb5c3849483ab4e113c093b
roshanlee <roshan26bhagat@gmail.com>
2021-06-09 23:26:41 +0530

    stire the result in dict

--- aritmaticComputation.sh
+++ aritmaticComputation.sh
@@ -2,7 +2,15 @@
 
 read -p "Enter a three number" a b c
 r1=$(($a+$b*$c))
-r2=$(($A*$b+$c))
+r2=$(($a*$b+$c))
 r3=$(($c+$a/$b))
 r4=$(($a%$b+$c))
+declare -A dict
+
+dict[1]=$r1
+dict[2]=$r2
+dict[3]=$r3
+dict[4]=$r4
+
+echo ${dict[@]}
 
